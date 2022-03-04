9f44a6abec17e179f318b67243ec18faf08a7c55
Your Name <Gustavo Orelio>
2022-03-03 22:38:39 -0300

    App consultoria pronto

--- atm_consultoria/lib/Home.dart
+++ atm_consultoria/lib/Home.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'TelaCliente.dart';
+import 'TelaContato.dart';
+import 'TelaEmpresa.dart';
+import 'TelaServico.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,8 +14,81 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  void _abrirEmpresa(){
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context)=> TelaEmpresa() ));
+  }
+
+  void _abrirServico(){
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context)=> TelaServico() ));
+  }
+
+  void _abrirCliente(){
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context)=> TelaCliente() ));
+  }
+
+  void _abrirContato(){
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context)=> TelaContato() ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("ATM Consultoria"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("images/logo.png"),
+            Padding(
+              padding: EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: _abrirEmpresa,
+                    child: Image.asset("images/menu_empresa.png"),
+                  ),
+                  GestureDetector(
+                    onTap: _abrirServico,
+                    child: Image.asset("images/menu_servico.png"),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: _abrirCliente,
+                    child: Image.asset("images/menu_cliente.png"),
+                  ),
+                  GestureDetector(
+                    onTap: _abrirContato,
+                    child: Image.asset("images/menu_contato.png"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
