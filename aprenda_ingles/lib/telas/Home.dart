bc95d88913dba8b118f8b57ea1773b9ca693a9d0
Your Name <Gustavo Orelio>
2022-03-23 23:14:34 -0300

    Configurando temas & abas

---
+++ aprenda_ingles/lib/telas/Home.dart
@@ -0,0 +1,65 @@
+import 'package:aprenda_ingles/telas/Bichos.dart';
+import 'package:aprenda_ingles/telas/Numeros.dart';
+import 'package:aprenda_ingles/telas/Vogais.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff795548),
+        title: Text("Aprenda inglês"),
+        bottom: TabBar(
+          indicatorWeight: 4,
+          indicatorColor: Colors.white,
+          labelStyle: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+          controller: _tabController,
+          tabs: [
+            Tab(
+              text: "Animais",
+            ),
+            Tab(
+              text: "Números",
+            ),
+            Tab(
+              text: "Vogais",
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Bichos(),
+          Numeros(),
+          Vogais(),
+        ],
+      ),
+    );
+  }
+}
